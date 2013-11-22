5fa248f91be0f5e631da0a18d8fa2c9cc2d6e374
Frank Sensui <fsensui@gmail.com>
2013-11-22 02:03:16 -0200

    Primeiro commt

---
+++ Conta-class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of conta
+ *
+ * @author Frank
+ */
+abstract class Conta {
+    //put your code here
+}
---
+++ teste.php
@@ -0,0 +1,10 @@
+<?php
+
+include("Carro-class.php");
+
+$c4 = new Carro();
+
+$i30 = new Carro();
+
+
+
